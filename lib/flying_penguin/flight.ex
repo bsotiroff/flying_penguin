3c7b18e8ff3b61aafd796fcbabe117600d170564
Brandon Sotiroff <bsotiroff@thecitybase.com>
2022-03-07 06:31:58 -0600

    issue #64 remove last db action

--- lib/flying_penguin/flight.ex
+++ lib/flying_penguin/flight.ex
@@ -4,28 +4,9 @@ defmodule FlyingPenguin.Flight do
   """
 
   import Ecto.Query, warn: false
-  alias FlyingPenguin.Repo
 
   alias FlyingPenguin.Flight.Search
 
-
-  @doc """
-  Creates a search.
-
-  ## Examples
-
-      iex> create_search(%{field: value})
-      {:ok, %Search{}}
-
-      iex> create_search(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def create_search(attrs \\ %{}) do
-    %Search{}
-    |> Search.changeset(attrs)
-    |> Repo.insert()
-  end
 
   @doc """
   Returns an `%Ecto.Changeset{}` for tracking search changes.
