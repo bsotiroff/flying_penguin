4e09f767536c17422f1cba1a6e0c0c743fd7ffa9
Olga Shiryaeva <oshiryaeva@protonmail.com>
2022-04-18 18:05:18 -0500

    Clean up

--- config/dev.exs
+++ config/dev.exs
@@ -27,16 +27,24 @@ config :flying_penguin, FlyingPenguinWeb.Endpoint,
   debug_errors: true,
   secret_key_base: System.get_env("FLYING_PENGUIN_KEY_BASE"),
   watchers: [
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
-    npx: [
-      "tailwindcss",
-      "--input=css/app.css",
-      "--output=../priv/static/assets/app.css",
-      "--postcss",
-      "--watch",
-      cd: Path.expand("../assets", __DIR__)
-    ]
+    # esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    # npx: [
+    #   "tailwindcss",
+    #   "--input=css/app.css",
+    #   "--output=../priv/static/assets/app.css",
+    #   "--postcss",
+    #   "--watch",
+    #   cd: Path.expand("../assets", __DIR__)
+    # ]
+    # node: [
+      # "node_modules/webpack/bin/webpack.js",
+      # "--mode",
+      # "development",
+      # "--watch-stdin",
+      # cd: Path.expand("../assets", __DIR__)
+    # ]
   ]
 
 # ## SSL Support
--- mix.exs
+++ mix.exs
@@ -33,16 +33,16 @@ defmodule FlyingPenguin.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.6.2"},
+      {:phoenix, "~> 1.6.5"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.17"},
+      {:phoenix_live_view, "~> 0.17.5"},
       {:floki, ">= 0.30.0"},
       {:phoenix_live_dashboard, "~> 0.6"},
-      {:esbuild, "~> 0.2", runtime: Mix.env() == :dev},
+      {:esbuild, "~> 0.4", runtime: Mix.env() == :dev},
       {:swoosh, "~> 1.3"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
